e0acade6bc60ed6e0a9dbc7650bbb5543d2fb5d4
louisrubin <miguel10.rbn@gmail.com>
2025-08-17 16:24:16 -0300

    unistall 'Switch' package

--- src/components/Label/LabelWithSwitch.tsx
+++ src/components/Label/LabelWithSwitch.tsx
@@ -1,19 +1,18 @@
-import { StyleSheet, View, ViewStyle } from "react-native";
+import { StyleSheet, Switch, View, ViewStyle } from "react-native";
 import TitleX from "../TitleX";
 import { useSettings } from "../../hooks/SettingsContext";
 import { Colors } from "../../constants/colors";
-import { Switch } from "react-native-switch";
 
 interface SwitchProp {
     title: string;
     value: boolean;
     disabled?: boolean;
     onValueChange?: () => void;
-    size?: number;
-    positionCircle?: number;
+    // size?: number;
+    // positionCircle?: number;
     // right?: number;
-    bgActiveColor?: string;
-    bgInactiveColor?: string;
+    // bgActiveColor?: string;
+    // bgInactiveColor?: string;
     styleContainer?: ViewStyle;
 }
 
@@ -21,11 +20,11 @@ export default function LabelWithSwitch( {
     title, 
     value, 
     disabled = false,
-    size = 22,
-    positionCircle = 3,
+    // size = 22,
+    // positionCircle = 3,
     // right = 3,
-    bgActiveColor,
-    bgInactiveColor,
+    // bgActiveColor,
+    // bgInactiveColor,
     onValueChange = () => {},
     styleContainer,
 } : SwitchProp){
@@ -34,21 +33,21 @@ export default function LabelWithSwitch( {
     
     return(
         <View style={[styles.container, styleContainer]}>
-            <TitleX text={title} style={{ width: "50%", color: ThemeValue.text}} />
+            <TitleX text={title} style={{ color: ThemeValue.text}} />
 
-            <Switch 
-                value={value} 
+            <Switch
+                value={value}
                 onValueChange={onValueChange}
                 disabled={disabled}
-                activeText=""
-                inActiveText=""
-                circleSize={size}
+                
 
-                backgroundActive={bgActiveColor ?? Colors["dark"].icon}
-                backgroundInactive={bgInactiveColor ?? ThemeValue.lineColor}
-
-                switchRightPx={positionCircle}
-                switchLeftPx={positionCircle}
+                trackColor={{ 
+                    true: Colors["dark"].icon,
+                    false: ThemeValue.lineColor 
+                }}
+                thumbColor="#ddd" // color del círculo
+                ios_backgroundColor={ThemeValue.marronText}
+                style={styles.switch}
             />
         </View>
     )
@@ -62,5 +61,8 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
         // backgroundColor: "tomato"
+    },
+    switch: {
+        transform: [{ scaleX: 1.35 }, { scaleY: 1.35 }], // agrandar el switch
     },
 });
